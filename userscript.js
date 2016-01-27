77bf0bc184f245f5369fd98e214dd7267ecd9349
Zach Himsel <zach@himsel.net>
2016-01-27 15:50:27 -0500

    Comments

--- userscript.js
+++ userscript.js
@@ -1,14 +1,14 @@
-window.fluid.dockBadge = "";
+window.fluid.dockBadge = ""; // set our initial, blank dockBadge
 setTimeout(updateDockBadge, 1000);
 setTimeout(updateDockBadge, 3000);
-setInterval(updateDockBadge, 1000);
+setInterval(updateDockBadge, 1000); // set how often we check for unread items
 
 
 function updateDockBadge() {
     var inbox = document.getElementsByClassName("has-newNotifications");
     if (Object.keys(inbox).length > 1) {
-        window.fluid.dockBadge = "•";
+        window.fluid.dockBadge = "•"; // unread items
     } else {
-        window.fluid.dockBadge = "";
+        window.fluid.dockBadge = ""; // no unread items
     }
 }
